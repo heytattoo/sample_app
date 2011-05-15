d69f533d8b54c060d8caa29f68c155681b4d10c3
John Lawler <jplawler@gmail.com>
2011-05-15 14:29:38 -0700

    about to start Friendly Forwarding implementation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,10 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def signed_in?
     !current_user.nil?
   end
@@ -20,6 +24,10 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
   end
 
   private
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -44,10 +44,11 @@ describe "Users" do
 
     describe "failure" do
       it "should not sign a user in" do
-        visit signin_path
-        fill_in :email,    :with => ""
-        fill_in :password, :with => ""
-        click_button
+        empty_user = User.new(:email => "", 
+                              :password => "",
+                              :password_confirmation => "",
+                              :name => "")
+        integration_sign_in(empty_user)
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
@@ -55,10 +56,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
